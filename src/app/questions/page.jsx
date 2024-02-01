c1b5075ce06602ba2cecf17312a31524da6ec8d2
stoll <stoll@uoguelph.ca>
2024-02-01 14:42:03 -0500

    fixed stuff2

--- src/app/questions/page.jsx
+++ src/app/questions/page.jsx
@@ -2,10 +2,7 @@
 import Image from "next/image";
 import Link from "next/link"
 import styles from "../page.module.css";
-import { sayHello } from "../layout";
 import {useState} from "react";
-import { useNavigate } from "react-router-dom";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function Questions() {
   const duck = require("..//duck.png");
